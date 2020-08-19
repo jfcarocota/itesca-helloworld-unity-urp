c2f288719bb527384c40348da722401c62ecf01d
jfcarocota <jfcarocota@gmail.com>
2020-08-18 17:32:22 -0700

    basic translate and animn idle, walk

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField]
+    float moveSpeed = 2f;
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //movement 3d
+        Movement();
+        anim.SetFloat("movement", Mathf.Abs(Axis.magnitude));
+    }
+
+    void Movement()
+    {
+        transform.Translate( Axis * Time.deltaTime * moveSpeed);
+    }
+
+    Vector3 Axis => new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+}
