d974809f41f181cc76a3fef52f4c50051d7f6edf
jfcarocota <jfcarocota@gmail.com>
2020-08-20 17:35:50 -0700

    ui collectable

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    TextMeshProUGUI textMesh;
 
     [SerializeField]
     float moveSpeed = 2f;
 
     Animator anim;
+
+    int score;
 
     // Start is called before the first frame update
     void Start()
@@ -21,13 +26,42 @@ public class Player : MonoBehaviour
     {
         //movement 3d
         Movement();
-        anim.SetFloat("movement", Mathf.Abs(Axis.magnitude));
+        anim.SetFloat("movement", AxisMagnitudeAbs);
     }
 
     void Movement()
     {
-        transform.Translate( Axis * Time.deltaTime * moveSpeed);
+        if(IsMoving)
+        {
+            transform.Translate( Vector3.forward * Time.deltaTime * moveSpeed);
+            transform.rotation = Quaternion.LookRotation(Axis.normalized);
+        }
     }
 
+    /// <summary>
+    /// Retunrs the axis with H input and V Input.
+    /// </summary>
+    /// <returns></returns>
     Vector3 Axis => new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+    /// <summary>
+    /// Check if player is moving with inputs H and V.
+    /// </summary>
+    bool IsMoving => AxisMagnitudeAbs > 0;
+
+    /// <summary>
+    /// Returns the magnitude of the Axis with inputs H and V.
+    /// </summary>
+    /// <returns></returns>
+    float AxisMagnitudeAbs => Mathf.Abs(Axis.magnitude);
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if(other.CompareTag("Collectable"))
+        {
+            score++;
+            textMesh.text = $"Score: {score}";
+            Destroy(other.gameObject);
+        }   
+    }
 }
