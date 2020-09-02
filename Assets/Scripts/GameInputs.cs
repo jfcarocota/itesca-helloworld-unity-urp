85baa6045635ba86c9f95b46ccf02d12c1ed8db3
jfcarocota <jfcarocota@gmail.com>
2020-09-01 17:49:08 -0700

    lambert model lidht

---
+++ Assets/Scripts/GameInputs.cs
@@ -0,0 +1,469 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/GameInputs.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @GameInputs : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @GameInputs()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""GameInputs"",
+    ""maps"": [
+        {
+            ""name"": ""Land"",
+            ""id"": ""6af0ee2b-1821-43e9-b431-5e52dd2d4d1a"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""a663de6c-514c-4483-ac0b-66f2ae459e9a"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump "",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""40271c18-3b76-4616-a570-0fb994c37213"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""wasd"",
+                    ""id"": ""7837f436-e823-4449-aafa-722efb484ed8"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""Up"",
+                    ""id"": ""11fd39d8-4ed9-42c6-af54-0111f044401e"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""PC"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Down"",
+                    ""id"": ""ee6b214f-238a-49d4-92d1-ee944a16e501"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""PC"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Left"",
+                    ""id"": ""30f67ddf-9236-4e1a-aa0f-8e7874e16822"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""PC"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Right"",
+                    ""id"": ""18effbe7-238a-4b94-a158-ca9e27648418"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""PC"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""arrows"",
+                    ""id"": ""12fd4a0e-025b-4b12-a11d-0c621768532d"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""c117b8ea-a232-454e-8d3b-33d517df0196"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""PC"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""3155747a-f8a0-4530-bef5-b5cc6baa0a8e"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""PC"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""318db4fb-de32-42e4-9314-8b93e283d2ba"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""PC"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""70170aaf-6840-4cfb-a179-d030b2c4da33"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""PC"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Left Stick"",
+                    ""id"": ""f10edeaf-427a-43c0-bcb0-1255161d16fc"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""25aa4592-c719-483c-ade3-f5ce11cca324"",
+                    ""path"": ""<XInputController>/leftStick/up"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""XBOX"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""7f0cf2a1-e3da-4d20-b0fc-a08e5f368945"",
+                    ""path"": ""<XInputController>/leftStick/down"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""XBOX"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""0074f830-a166-4ed4-9b70-7af0d31c5178"",
+                    ""path"": ""<XInputController>/leftStick/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""XBOX"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""9c0f2a31-ac0f-4018-a27e-79f010ebf7b3"",
+                    ""path"": ""<XInputController>/leftStick/right"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""XBOX"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""140d8a6a-0a87-48f1-8a69-dc3a9805ed8e"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""a5d80d5a-3166-4fd4-ad7e-03ffa8e9b471"",
+                    ""path"": ""<AndroidJoystick>/stick/up"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Android"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""0d1ab9c2-5316-4125-8a72-f4a48d8b6bd7"",
+                    ""path"": ""<AndroidJoystick>/stick/down"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Android"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""291f46ca-64ff-46d0-bef6-2f91d2f7ae4e"",
+                    ""path"": ""<AndroidJoystick>/stick/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Android"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""aa6fac02-38c8-43d2-b08a-148058c69e58"",
+                    ""path"": ""<AndroidJoystick>/stick/right"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Android"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Button With One Modifier"",
+                    ""id"": ""3094a01a-7fc8-427b-9ad0-c58173cd272d"",
+                    ""path"": ""ButtonWithOneModifier"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump "",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""modifier"",
+                    ""id"": ""60fcd215-4639-4cdf-97e5-3a309d1a3bb5"",
+                    ""path"": """",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Android"",
+                    ""action"": ""Jump "",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""button"",
+                    ""id"": ""2bf3c88d-70aa-4101-b44b-37e32fc64417"",
+                    ""path"": ""<Touchscreen>/touch0/tap"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Android"",
+                    ""action"": ""Jump "",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""70ae5e65-1480-45b2-bab3-38990823c924"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""PC"",
+                    ""action"": ""Jump "",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""PC"",
+            ""bindingGroup"": ""PC"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""XBOX"",
+            ""bindingGroup"": ""XBOX"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<XInputController>"",
+                    ""isOptional"": true,
+                    ""isOR"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""Android"",
+            ""bindingGroup"": ""Android"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<AndroidJoystick>"",
+                    ""isOptional"": true,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Touchscreen>"",
+                    ""isOptional"": true,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // Land
+        m_Land = asset.FindActionMap("Land", throwIfNotFound: true);
+        m_Land_Move = m_Land.FindAction("Move", throwIfNotFound: true);
+        m_Land_Jump = m_Land.FindAction("Jump ", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Land
+    private readonly InputActionMap m_Land;
+    private ILandActions m_LandActionsCallbackInterface;
+    private readonly InputAction m_Land_Move;
+    private readonly InputAction m_Land_Jump;
+    public struct LandActions
+    {
+        private @GameInputs m_Wrapper;
+        public LandActions(@GameInputs wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Land_Move;
+        public InputAction @Jump => m_Wrapper.m_Land_Jump;
+        public InputActionMap Get() { return m_Wrapper.m_Land; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(LandActions set) { return set.Get(); }
+        public void SetCallbacks(ILandActions instance)
+        {
+            if (m_Wrapper.m_LandActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_LandActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_LandActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_LandActionsCallbackInterface.OnMove;
+                @Jump.started -= m_Wrapper.m_LandActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_LandActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_LandActionsCallbackInterface.OnJump;
+            }
+            m_Wrapper.m_LandActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+            }
+        }
+    }
+    public LandActions @Land => new LandActions(this);
+    private int m_PCSchemeIndex = -1;
+    public InputControlScheme PCScheme
+    {
+        get
+        {
+            if (m_PCSchemeIndex == -1) m_PCSchemeIndex = asset.FindControlSchemeIndex("PC");
+            return asset.controlSchemes[m_PCSchemeIndex];
+        }
+    }
+    private int m_XBOXSchemeIndex = -1;
+    public InputControlScheme XBOXScheme
+    {
+        get
+        {
+            if (m_XBOXSchemeIndex == -1) m_XBOXSchemeIndex = asset.FindControlSchemeIndex("XBOX");
+            return asset.controlSchemes[m_XBOXSchemeIndex];
+        }
+    }
+    private int m_AndroidSchemeIndex = -1;
+    public InputControlScheme AndroidScheme
+    {
+        get
+        {
+            if (m_AndroidSchemeIndex == -1) m_AndroidSchemeIndex = asset.FindControlSchemeIndex("Android");
+            return asset.controlSchemes[m_AndroidSchemeIndex];
+        }
+    }
+    public interface ILandActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,15 +10,56 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     float moveSpeed = 2f;
+    [SerializeField]
+    float jumpForce = 5f;
 
     Animator anim;
 
     int score;
 
+    GameInputs gameInpunts;
+
+    Rigidbody rb;
+
+    [SerializeField]
+    Color rayColor = Color.magenta;
+    [SerializeField, Range(0.1f, 10f)]
+    float rayDistance = 5;
+    [SerializeField]
+    LayerMask groundLayer;
+    [SerializeField]
+    Transform rayTransform;
+
+    void Awake()
+    {
+        gameInpunts = new GameInputs();
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();
+        gameInpunts.Land.Jump.performed += _=> Jump();
+    }
+
+    void Jump()
+    {
+        if(IsGrounding)
+        {
+            anim.SetTrigger("jump");
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    void OnEnable()
+    {
+        gameInpunts.Enable();
+    }
+
+    void OnDisable()
+    {
+        gameInpunts.Disable();
     }
 
     // Update is called once per frame
@@ -26,7 +67,12 @@ public class Player : MonoBehaviour
     {
         //movement 3d
         Movement();
+    }
+
+    void LateUpdate()
+    {
         anim.SetFloat("movement", AxisMagnitudeAbs);
+        anim.SetBool("ground", IsGrounding);
     }
 
     void Movement()
@@ -34,7 +80,7 @@ public class Player : MonoBehaviour
         if(IsMoving)
         {
             transform.Translate( Vector3.forward * Time.deltaTime * moveSpeed);
-            transform.rotation = Quaternion.LookRotation(Axis.normalized);
+            transform.rotation = Quaternion.LookRotation(new Vector3(Axis.x, 0f, Axis.y));
         }
     }
 
@@ -42,7 +88,7 @@ public class Player : MonoBehaviour
     /// Retunrs the axis with H input and V Input.
     /// </summary>
     /// <returns></returns>
-    Vector3 Axis => new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+    Vector2 Axis => gameInpunts.Land.Move.ReadValue<Vector2>();
 
     /// <summary>
     /// Check if player is moving with inputs H and V.
@@ -55,6 +101,8 @@ public class Player : MonoBehaviour
     /// <returns></returns>
     float AxisMagnitudeAbs => Mathf.Abs(Axis.magnitude);
 
+    bool IsGrounding => Physics.Raycast(rayTransform.position, -transform.up, rayDistance, groundLayer);
+
     void OnTriggerEnter(Collider other) 
     {
         if(other.CompareTag("Collectable"))
@@ -63,5 +111,11 @@ public class Player : MonoBehaviour
             textMesh.text = $"Score: {score}";
             Destroy(other.gameObject);
         }   
+    }
+
+    void OnDrawGizmosSelected() 
+    {
+        Gizmos.color = rayColor;
+        Gizmos.DrawRay(rayTransform.position, -transform.up * rayDistance);    
     }
 }
